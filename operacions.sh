06b0f496014d09d991d9804b4ae49d6e82546aba
Alonso <avgottschling@esliceu.net>
2026-04-14 18:41:48 +0200

    Variable i entrada per teclat

---
+++ operacions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Variables són "paraules" que contenen un valor
+NOM="alonso"
+#read serveix per llegir info del teclat
+read -p "Introdueix un nom: " NOM
+
+echo "El teu nom és ${NOM}"
