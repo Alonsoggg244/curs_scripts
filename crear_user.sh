cbaacc2673c167ece56fac26e033bf5890802460
alonso <avgottschling@esliceu.net>
2026-04-20 15:49:07 +0200

    Script per crear usuaris

--- crear_user.sh
+++ crear_user.sh
@@ -4,10 +4,10 @@ function usage(){
     echo
     echo "has de ser root"
     echo "l'usuari no és pot repetir"
-    exit 1    
+    exit 1 #indica que hi ha hagut una errada
 }
 
-#Script per crear usuari
+#Script per crear un usuari 
 
 #primer comprovam si som l'usuari root
 if [[ ${UID} -ne 0 ]]
@@ -15,8 +15,32 @@ then
     usage
 fi
 
+read -p "Introdueix el nom complet: " COMMENTS
+#demanam el nom de l'usuari
 read -p "Introdueix el nom d'usuari: " USER_NAME
+#demanam el password
+read -p "Introdueix  el password: " PASSWORD
 
 
+#crearíem l'usuari
+useradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
+#control de la darrera comanda
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada creant l'usuari"
+    exit 1
+fi
 
-echo "creant l'usuari ${USER_NAME}"
+#canvi de password
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+#comprovam si el canvi de password ha anat bé.
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada canviant password"
+    exit 1
+fi
+
+#fer caducar el password
+passwd -e ${USER_NAME}
+
+exit 0
