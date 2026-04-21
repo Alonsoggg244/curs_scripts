fafb8f84e990a82f3e13afe1d7b1f8a6fcf32c1a
alonso <avgottschling@esliceu.net>
2026-04-21 17:32:30 +0200

    Video 20 iterar arguments amb FOR

---
+++ 009_parametres.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Ens mostra el scrpt executat
+echo "El nom del script és ${0}"
+
+#mostrarem el PATH (RUTA) i el filename (NOM DEL FITXER) del script
+echo "el PATH: $(dirname ${0}) el fitxer seria $(basename ${0})"
+
+#per determinar es número de parametres
+NUMERO_DE_PARAMETRES=${#}
+
+if [[ ${NUMERO_DE_PARAMETRES} -eq 0 ]]
+then
+    echo "USAGE:${0} USER_NAME [USER_NAME] ..."
+    exit
+fi
+
+#${*} -->mostra conjunt de paràmetres en una sola paraula "${1} ${2} ${3}.....${n}"
+echo "${*}"
+echo
+#${@} -->mostra conjunt de parametres en paraules diferents "${1}" "${2}" "${3}" ..... "${n}"
+echo "${@}"
+
+#estructura de cotrol FOR per iterar els parametres
+for USER_NAME in "${@}"
+do
+    PASSWORD=$(date +%s%N | sha256sum | head -c10 )
+    echo "${USER_NAME}:${PASSWORD}"
+done
